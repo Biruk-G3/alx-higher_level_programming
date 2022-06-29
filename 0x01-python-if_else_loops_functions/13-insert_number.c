26780cb6d649586db579d514adfde980e699677b
Biruk Gelelcha <byafet3@gmail.com>
2022-06-29 19:06:13 +0000

    hot fix

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,42 +1,51 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * insert_node - inserts a number into a sorted singly linked list.
- * @head: list head
- * @number: number to store in the new node
- * Return: pointer to the new node
- */
+  * insert_node - Inserts a number into a sorted singly linked list
+  * @head: The head of the sorted singly linked list
+  * @number: The number to inserts in the singly linked list
+  *
+  * Return: The singly linked list with the new number added
+  */
 listint_t *insert_node(listint_t **head, int number)
 {
-        listint_t *runner;
-        listint_t *new;
+	listint_t *current = NULL, *new_node = NULL, *temp = NULL;
 
-        runner = *head;
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-        new = malloc(sizeof(listint_t));
-        if (new == NULL)
-                return (NULL);
-        new->n = number;
+	new_node->n = number;
+	if (*head)
+	{
+		current = *head;
+		if (number <= current->n)
+		{
+			new_node->next = current;
+			*head = new_node;
+		}
+		else
+		{
+			while (current->next)
+			{
+				if (number <= current->next->n)
+				{
+					temp = current->next;
+					current->next = new_node;
+					new_node->next = temp;
+					return (*head);
+				}
 
-        if (*head == NULL || (*head)->n > number)
-        {
-                new->next = *head;
-                *head = new;
-                return (new);
-        }
-
-        while (runner->next != NULL)
-        {
-                if ((runner->next)->n >= number)
-                {
-                        new->next = runner->next;
-                        runner->next = new;
-                        return (new);
-                }
-                runner = runner->next;
-        }
-
-        new->next = NULL;
-        runner->next = new;
-        return (new);
+				current = current->next;
+			}
+			temp = current->next;
+			current->next = new_node;
+			new_node->next = temp;
+		}
+		return (*head);
+	}
+	new_node->next = NULL;
+	*head = new_node;
+	return (*head);
 }
