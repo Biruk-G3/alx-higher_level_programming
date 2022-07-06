f6d6c0e351bc38ca673f72a20d4cfd85cc87abed
Biruk Gelelcha <byafet3@gmail.com>
2022-07-06 12:24:24 +0000

    higest score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
-        return None
-     
+        return None     
     else:
         return max(a_dictionary, key= lambda x: a_dictionary[x])
         #alternetive way best = max(a_dictionary, key=a_dictionary.get
