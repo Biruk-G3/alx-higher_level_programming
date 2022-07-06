2dbd03ec6cf11b788cf366a5475b43ad9ee11638
Biruk Gelelcha <byafet3@gmail.com>
2022-07-06 09:58:52 +0000

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]): 
-    sqr_matrix = list(map(lambda x:list(map(lambda y: (y**2), x)), matrix ))
-    # number ** 2 for number in list1
-    return sqr_matrix
+def square_matrix_simple(matrix=[]):
+    return list(map(lambda x:list(map(lambda y: (y**2), x)), matrix ))
