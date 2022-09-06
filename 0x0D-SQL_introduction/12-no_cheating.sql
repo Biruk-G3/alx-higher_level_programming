5b11167985b7aae1f1dc3701c98ae49690ae38b5
Biruk Gelelcha <byafet3@gmail.com>
2022-09-06 10:59:45 -0700

    making change updating and deleting

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- updating a value of a record using there name
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";
---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- delete a record based on there score 
+DELETE FROM `second_table`
+WHERE `score` <= 5;
