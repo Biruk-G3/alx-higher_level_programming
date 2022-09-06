edf48649b198ac4b89ba63318f6b3c8efc90db75
Biruk Gelelcha <byafet3@gmail.com>
2022-09-06 10:45:26 -0700

    fix

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
--- creat a TABLE and insert multiple values to the created DB second_table.
-CREATE TABLE IF NOT EXISTS `seccond_table` (`id` INT, `name` VARCHAR(256), `score` INT);
-INSERT INTO `second_table` (`id`,`name`,`score`) VALUES (1, "John", 10);
-INSERT INTO `second_table` (`id`,`name`,`score`) VALUES (2, "Alex", 3);
-INSERT INTO `second_table` (`id`,`name`,`score`) VALUES (3, "Bob", 14);
-INSERT INTO `second_table` (`id`,`name`,`score`) VALUES (4, "George",8);
+-- Creates and fills a table second_table with attributes id, name and score.
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
