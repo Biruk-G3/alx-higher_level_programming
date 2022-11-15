34a18827f2b08cced5ea78af1fef8c626bd71e2b
BirukG <byafet3@gmail.com>
2022-11-15 16:28:58 +0000

    write

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,13 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 const args = process.argv;
-fs.readFile(args[2], (err, data) => {
+fs.writeFile(args[2], args[3], (err) => {
   if (err) console.log(err);
-  else console.log(data.toString());
-});#!/usr/bin/node
-const fs = require('fs');
-const args = process.argv;
-fs.readFile(args[2], (err, data) => {
-  if (err) console.log(err);
-  else console.log(data.toString());
 });
